e26035dbb1c1cdb89ffdf6ba7fcb1488feb13b83
rasyidrmhd <rasyidrmuhammad@gmail.com>
2023-08-21 14:22:16 +0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = styled.header`
+  display: block;
+`;
+
+export default () => (
+  <Header>
+    <h1>Wool Icon</h1>
+  </Header>
+);
